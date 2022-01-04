2b226f03fa5beaed11bd78f9f116e5f5bec0c5b0
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-01-04 21:46:29 +0200

    Exe

---
+++ EntityFrameworkCore/02.ORMFundamentals-Exe/MiniORMCore/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using MiniORM.app.Data;
+    using MiniORM.app.Data.Entities;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "Server=.;Database=MiniORM;Integrated Security =true";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
