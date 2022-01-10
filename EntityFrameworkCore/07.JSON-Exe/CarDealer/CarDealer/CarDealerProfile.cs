0976f45332bb0e2f3932fad2b86b98bc4bdef02f
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-01-10 09:15:21 +0200

    Exe

---
+++ EntityFrameworkCore/07.JSON-Exe/CarDealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using CarDealer.DTO;
+    using CarDealer.Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SuppliersDTO, Supplier>();
+            CreateMap<PartsDTO, Part>();
+            CreateMap<CarsDTO, Car>();
+            CreateMap<CustomersDTO, Customer>();
+            CreateMap<SalesDTO, Sale>();
+        }
+    }
+}
---
+++ EntityFrameworkCore/07.JSON-Exe/CarDealer/CarDealer/DTO/SuppliersDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CarDealer.DTO
+{
+    public class SuppliersDTO
+    {
+        public string Name { get; set; }
+
+        public bool IsImporter { get; set; }
+    }
+}
